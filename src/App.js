bea7245392626d7385657def92e112d0b80bd599
Anthony Salgado <anthonyasalgado@gmail.com>
2021-04-16 23:58:24 -0700

    setting up remove function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import {Table, Button} from 'reactstrap'
+import { Table, Button } from 'reactstrap';
+import { faImage, faThumbsUp, faThumbsDown, faMoneyCheckAlt, faSearchDollar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class App extends Component {
     state = { 
@@ -13,20 +15,25 @@ class App extends Component {
                 "Date" : "08/21/2019"
             },
             {
-                "id" : "101",
+                "id" : "200",
                 "Vendor" : "Hankook",
                 "Amount" : "$18,000",
                 "Invoice" : "1234",
                 "Date" : "08/21/2019"
             },
-            // {
-            //     "id" : "102",
-            //     "Vendor" : "Hankook",
-            //     "Amount" : "$18,000",
-            //     "Invoice" : "1234",
-            //     "Date" : "08/21/2019"
-            // },
+            {
+                "id" : "300",
+                "Vendor" : "Hankook",
+                "Amount" : "$18,000",
+                "Invoice" : "1234",
+                "Date" : "08/21/2019"
+            },
         ]
+     }
+
+     remove(id) {
+         let updatedInvoices = [...this.invoices].filter( x => x.id !== id);
+         this.setState({invoices : updatedInvoices});
      }
 
     render() { 
@@ -44,6 +51,11 @@ class App extends Component {
                 <td>{invoice.Amount}</td>
                 <td>{invoice.Invoice}</td>
                 <td>{invoice.Date}</td>
+                <td><Button className="btn btn-lg btn-success" onClick={() => this.remove(invoice.id)}> OK </Button></td>
+                <td><Button className="btn btn-lg btn-danger" onClick={() => this.remove(invoice.id)}> NOK </Button></td>
+                <td><Button className="btn btn-lg btn-info" onClick={() => this.remove(invoice.id)}> 50% </Button></td>
+                <td><Button className="btn btn-lg btn-warning" onClick={() => this.remove(invoice.id)}> ?? </Button></td>
+                <td><Button className="btn btn-lg btn-info" onClick={() => this.remove(invoice.id)}> Image </Button></td>
             </tr>
             )
 
@@ -66,7 +78,7 @@ class App extends Component {
                                 <th>Image</th>
                             </thead>
                             <tbody>
-                                {this.state.invoices.length === 0 ? <td colSpan="9">All caught up!</td> : invoice}
+                                {this.state.invoices.length === 0 ? <td colSpan="9">All caught up!</td> : invoices}
                             </tbody>
                         </Table>
                     </div>
